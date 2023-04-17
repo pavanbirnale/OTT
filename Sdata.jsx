8da65247b2925b9a4601107ea430a5a0fe8dfdfb
pawan birnale <birnalepavan@gmail.com>
2023-04-17 13:15:50 +0530

    first commit

---
+++ Sdata.jsx
@@ -0,0 +1,34 @@
+const Sdata = [
+    {
+        sname:"GASLIGHT",
+        imgsrc:"https://img1.hotstarext.com/image/upload/f_auto/sources/r1/cms/prod/4969/1494969-i-58a953c8f194",
+        title:"Hotstar Original",
+        link: "https://www.hotstar.com/in/movies/gaslight/1260135709/watch?filters=content_type%3Dmovie",
+    },
+    {
+        sname:"HAR HAR MAHADEV",
+        imgsrc:"https://img1.hotstarext.com/image/upload/f_auto/sources/r1/cms/prod/8976/1378976-i-7e26d648962a",
+        title:"hotstar free",
+        link: "https://www.hotstar.com/in/shows/mahadev/12/watch?action=ctaNavigation&filters=content_type%3Depisode",
+    },
+    {
+        sname:"MAHABHARAT",
+        imgsrc:"https://img1.hotstarext.com/image/upload/f_auto/sources/r1/cms/prod/3132/1383132-i-011cefc50da9",
+        title:"Hotstar spiritual",
+        link: "https://www.hotstar.com/in/shows/mahabharat/435/mahabharat/1000011769/watch?filters=content_type%3Depisode",
+    },
+    {
+        sname:"RADHA KRUSHNA",
+        imgsrc:"https://img1.hotstarext.com/image/upload/f_auto/sources/r1/cms/prod/3232/1423232-i-050f622bde39",
+        title:"Hotstar spiritual ",
+        link: "https://www.hotstar.com/in/shows/radhakrishn/1260000646/radhakrishn/1000282173/watch?filters=content_type%3Depisode",
+    },
+    {
+        sname:"RADHA KRUSHNA",
+        imgsrc:"https://img1.hotstarext.com/image/upload/f_auto/sources/r1/cms/prod/3232/1423232-i-050f622bde39",
+        title:"Hotstar spiritual ",
+        link: "https://www.hotstar.com/in/shows/radhakrishn/1260000646/radhakrishn/1000282173/watch?filters=content_type%3Depisode",
+    },
+  
+];
+export default Sdata;
